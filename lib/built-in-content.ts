7995855a9423c193d0cb8d49a89b34e580bab638
junginaha <junginaha@gmail.com>
2026-08-17 14:56:56 +0900

    8월 17일 경험형 시드를 공개 피드에 연결

--- lib/built-in-content.ts
+++ lib/built-in-content.ts
@@ -17,6 +17,7 @@ import { august12EditorialComments, august12EditorialPosts } from "./daily-edito
 import { august14EditorialComments, august14EditorialPosts } from "./daily-editorial-20260814";
 import { august15EditorialComments, august15EditorialPosts } from "./daily-editorial-20260815";
 import { august16EditorialComments, august16EditorialPosts } from "./daily-editorial-20260816";
+import { august17EditorialComments, august17EditorialPosts } from "./daily-editorial-20260817";
 import { august5MorningComments, august5MorningPosts } from "./morning-editorial-20260805";
 import { august6MorningComments, august6MorningPosts } from "./morning-editorial-20260806";
 import { august8MorningComments, august8MorningPosts } from "./morning-editorial-20260808";
@@ -50,6 +51,7 @@ function chooseUniquePosts(posts: EditorialPost[]) {
 }
 
 export const builtInPosts = chooseUniquePosts([
+  ...august17EditorialPosts,
   ...august16EditorialPosts,
   ...august15MorningPosts,
   ...august15EditorialPosts,
@@ -89,6 +91,7 @@ export function builtInPost(id: string) {
 export function builtInComments(id: string): EditorialComment[] {
   const merged = new Map<string, EditorialComment>();
   for (const comment of [
+    ...august17EditorialComments(id),
     ...august16EditorialComments(id),
     ...august15MorningComments(id),
     ...august15EditorialComments(id),
