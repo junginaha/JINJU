bc8bd29695b2112433f40c33b7fd28d6a294d22e
junginaha <junginaha@gmail.com>
2026-09-09 06:19:31 +0900

    Add September 9 editorial posts and scheduled comments

--- lib/built-in-content.ts
+++ lib/built-in-content.ts
@@ -39,6 +39,7 @@ import { september5EditorialComments, september5EditorialPosts } from "./daily-e
 import { september6EditorialComments, september6EditorialPosts } from "./daily-editorial-20260906";
 import { september7EditorialComments, september7EditorialPosts } from "./daily-editorial-20260907";
 import { september8EditorialComments, september8EditorialPosts } from "./daily-editorial-20260908";
+import { september9EditorialComments, september9EditorialPosts } from "./daily-editorial-20260909";
 import { august5MorningComments, august5MorningPosts } from "./morning-editorial-20260805";
 import { august6MorningComments, august6MorningPosts } from "./morning-editorial-20260806";
 import { august8MorningComments, august8MorningPosts } from "./morning-editorial-20260808";
@@ -81,6 +82,7 @@ export const builtInPosts = chooseUniquePosts([
   husbandHiddenDebtPost,
   ...september7EditorialPosts,
   ...september8EditorialPosts,
+  ...september9EditorialPosts,
   ...september6EditorialPosts,
   ...september5EditorialPosts,
   ...september4EditorialPosts,
@@ -144,6 +146,7 @@ export function builtInComments(id: string): EditorialComment[] {
     ...september7ExtraComments(id),
     ...september7EditorialComments(id),
     ...september8EditorialComments(id),
+    ...september9EditorialComments(id),
     ...september6EditorialComments(id),
     ...september5EditorialComments(id),
     ...september4EditorialComments(id),
