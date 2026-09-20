2e142f8351b04616e96e04bcfdd55f27b7a36894
junginaha <junginaha@gmail.com>
2026-09-21 06:14:27 +0900

    content: connect September 21 feed

--- lib/built-in-content.ts
+++ lib/built-in-content.ts
@@ -47,6 +47,7 @@ import { september13EditorialComments, september13EditorialPosts } from "./daily
 import { september14EditorialComments, september14EditorialPosts } from "./daily-editorial-20260914";
 import { september15EditorialComments, september15EditorialPosts } from "./daily-editorial-20260915";
 import { september20EditorialComments, september20EditorialPosts } from "./daily-editorial-20260920";
+import { september21EditorialComments, september21EditorialPosts } from "./daily-editorial-20260921";
 import { august5MorningComments, august5MorningPosts } from "./morning-editorial-20260805";
 import { august6MorningComments, august6MorningPosts } from "./morning-editorial-20260806";
 import { august8MorningComments, august8MorningPosts } from "./morning-editorial-20260808";
@@ -87,6 +88,7 @@ function chooseUniquePosts(posts: EditorialPost[]) {
 
 export const builtInPosts = chooseUniquePosts([
   husbandHiddenDebtPost,
+  ...september21EditorialPosts,
   ...september20EditorialPosts,
   ...september15EditorialPosts,
   ...september14EditorialPosts,
@@ -157,6 +159,7 @@ export function builtInPost(id: string) {
 export function builtInComments(id: string): EditorialComment[] {
   const merged = new Map<string, EditorialComment>();
   for (const comment of [
+    ...september21EditorialComments(id),
     ...september20EditorialComments(id),
     ...september15EditorialComments(id),
     ...september7ExtraComments(id),
